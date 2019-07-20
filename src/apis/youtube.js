dabe4c61becfa27d013f025482d2067b39e9cf5f
khashayar <khashayar.sasanni@hotmail.com>
2019-07-20 13:08:33 +0200

    git commit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -5,8 +5,9 @@ const KEY = 'AIzaSyD_q-77m-FYWHPbxwHXdM-ldSaWUsxD984';
 export default axios.create({ 
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
-        part: 'snippet',
+        part: 'snippet', 
+        type: 'video',
         maxResults: 5,
-        key: KEY
+        key: `${KEY}`
     }
 });
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -14,7 +14,7 @@ const VideoDetail = ({ video }) => {
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
-                <p4>{video.snippet.description}</p4>
+                <p>{video.snippet.description}</p>
             </div>
         </div>
     );
